672faf2deff53c82459a1047f8c755a9bf015eb8
AChirkova <leadenrain@yandex.ru>
2024-10-16 13:16:23 +0500

    add feed test

---
+++ src/services/slices/order/feed/mockData.ts
@@ -0,0 +1,35 @@
+export const mockedFeed = {
+  success: true,
+  orders: [
+    {
+      _id: '670f3476d829be001c77678d',
+      ingredients: [
+        '643d69a5c3f7b9001cfa093c',
+        '643d69a5c3f7b9001cfa0943',
+        '643d69a5c3f7b9001cfa093f',
+        '643d69a5c3f7b9001cfa0940',
+        '643d69a5c3f7b9001cfa093c'
+      ],
+      status: 'done',
+      name: 'Краторный space бессмертный метеоритный бургер',
+      createdAt: '2024-10-16T03:35:18.130Z',
+      updatedAt: '2024-10-16T03:35:18.958Z',
+      number: 56576
+    },
+    {
+      _id: '670eeca0d829be001c776748',
+      ingredients: [
+        '643d69a5c3f7b9001cfa093c',
+        '643d69a5c3f7b9001cfa093e',
+        '643d69a5c3f7b9001cfa093c'
+      ],
+      status: 'done',
+      name: 'Краторный люминесцентный бургер',
+      createdAt: '2024-10-15T22:28:48.291Z',
+      updatedAt: '2024-10-15T22:28:49.243Z',
+      number: 56575
+    }
+  ],
+  total: 2,
+  totalToday: 2
+};
