fb01a2442d3f71be7f519472216ca2f460a2b7a7
AChirkova <leadenrain@yandex.ru>
2024-11-25 18:47:52 +0500

    edit event type

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,20 +1,23 @@
-import { FC, SyntheticEvent, useState } from 'react';
+import { FC, FormEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
 import { loginUser } from '../../services/slices/auth/thunk';
 import { useDispatch } from '../../services/store';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Login: FC = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e: SyntheticEvent) => {
+  const from = location.state?.from || '/';
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     dispatch(loginUser({ email, password }));
-    navigate('/');
+    navigate(from, { replace: true });
   };
 
   return (
